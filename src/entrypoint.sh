16d013502bcdd3429f63dc11014bfcbcea73b2db
Andreas Alexopoulos <andalexo@gmail.com>
2023-03-16 20:11:36 +0200

    fixes help; adds input checks; adds exit code checks

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,7 +6,7 @@ BOLD=`tput bold`
 REV=`tput smso`
 
 function show_usage () {
-    echo -e "${BOLD}Basic usage:${NORM} entrypoint.sh [-vh] FIN_PERSONS FIN_HOMES SEED SCALING OUT_PATH"
+    echo -e "${BOLD}Basic usage:${NORM} entrypoint.sh [-vh] SYNPOP_CONFIG_XML TRACES OUT_PATH"
 }
 
 function show_help () {
@@ -14,15 +14,13 @@ function show_help () {
     show_usage
     echo -e "\n${BOLD}Required arguments:${NORM}"
     echo -e "${REV}SYNPOP_CONFIG_XML${NORM}\t the config xml from synpop"
-    echo -e "${REV}FIN_HOMES${NORM}\t the homes gpkg input file"
-    echo -e "${REV}SEED${NORM}\t the random seed"
-    echo -e "${REV}SCALING${NORM}\t the scaling factor"
+    echo -e "${REV}TRACES${NORM}\t the traces json from the jsprit to matsim connector"
     echo -e "${REV}OUT_PATH${NORM}\t the output path"\\n
     echo -e "${BOLD}Optional arguments:${NORM}"
     echo -e "${REV}-v${NORM}\tSets verbosity level"
     echo -e "${REV}-h${NORM}\tShows this message"
     echo -e "${BOLD}Examples:${NORM}"
-    echo -e "entrypoint.sh -v persons.xlsx homes.gpkg 1234 0.1 ./output/"
+    echo -e "entrypoint.sh -v sample-data/input/lead_2022_5pct_config.xml sample-data/input/traces.json sample-data/output/"
 }
 
 ##############################################################################
@@ -57,11 +55,18 @@ shift "$(($OPTIND -1))"
 leftovers=(${@})
 SYNPOP_CONFIG_XML=${leftovers[0]}
 TRACES=${leftovers[1]}
-OUT_PATH=${leftovers[2]}
+OUT_PATH=${leftovers[2]%/}
 
 ##############################################################################
 # Input checks                                                               #
 ##############################################################################
+if [ ! -f "${SYNPOP_CONFIG_XML}" ]; then
+     echo -e "Give a ${BOLD}valid${NORM} config input file\n"; show_usage; exit 1
+fi
+if [ ! -f "${TRACES}" ]; then
+     echo -e "Give a ${BOLD}valid${NORM} traces input file\n"; show_usage; exit 1
+fi
+
 if [ ! -d "${OUT_PATH}" ]; then
      echo -e "Give a ${BOLD}valid${NORM} output directory\n"; show_usage; exit 1
 fi
@@ -72,9 +77,11 @@ fi
 java -Xmx20g -cp /srv/app/java/target/lead-matsim-1.0.0.jar fr.irtx.lead.matsim.RunSimulation \
 	--config-path ${SYNPOP_CONFIG_XML} \
 	--output-path ${OUT_PATH}
-
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 echo "1: output"
-ls ${OUT_PATH}
+ls -la ${OUT_PATH}
 
 java -Xmx20G -cp /srv/app/java/target/lead-matsim-1.0.0.jar org.eqasim.core.scenario.cutter.RunScenarioCutter \
 	--config-path ${SYNPOP_CONFIG_XML} \
@@ -82,20 +89,22 @@ java -Xmx20G -cp /srv/app/java/target/lead-matsim-1.0.0.jar org.eqasim.core.scen
 	--output-path ${OUT_PATH} \
 	--prefix perimeter_ \
 	--config:plans.inputPlansFile ${OUT_PATH}/output_plans.xml.gz
-
+if [ $? -ne 0 ]; then
+    exit 2
+fi
 echo "2: output"
-ls ${OUT_PATH}
+ls -la ${OUT_PATH}
 
 java -Xmx20g -cp /srv/app/java/target/lead-matsim-1.0.0.jar fr.irtx.lead.matsim.RunSimulation \
     --config-path ${OUT_PATH}/perimeter_config.xml \
     --output-path ${OUT_PATH} \
     --freight-path ${TRACES}
-
+if [ $? -ne 0 ]; then
+    exit 3
+fi
 echo "3: output"
-ls ${OUT_PATH}
+ls -la ${OUT_PATH}
 
 # papermill /srv/app/src/congestion-analysis.ipynb /dev/null \
 #     -psimulation_output_path /home/ubuntu/irtx-matsim/output/output_${scenario} \
 #     -pkpi_path /home/ubuntu/irtx-matsim/output/congestion_${scenario}.json
-
-
